99e5071d43c3f8a15f14f992fb200f4da9524f12
Md Ahanaf Islam <ahanafislam@gmail.com>
2022-03-01 02:23:51 +0600

    Done basic search function and display in ui

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
+// Get HTML element by ID
+const getElement = id => document.getElementById(id);
 
+// Fetch Search Result
+const loadSearchResult = () => {
+    const searchValue = getElement('search-input').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data));
+}
+
+// Display Search Result
+const displaySearchResult = data => {
+    if(data.status) {
+        const result = getElement('result');
+        const phoneList = data.data;
+        phoneList.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card shadow p-2 mx-auto" style="width: 18rem;">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h6>${phone.brand}</h6>
+                        <h4 class="card-title">${phone.phone_name}</h4>
+                        <a href="#" class="btn bg-phone-arena">Details</a>
+                    </div>
+                </div>
+            `;
+            result.appendChild(div);
+        });
+    }
+    else {
+        console.log("Result not found");
+    }
+}
